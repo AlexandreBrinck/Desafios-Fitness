c8635f12ae1a1a71de67bca0da6d27344a2fd6c3
AlexandreBrinck <alexandrebrinck3@gmail.com>
2026-09-17 14:42:01 -0300

    Corrige conexão MySQL para Docker

--- src/main/java/br/com/desafiosfitness/config/MysqlSingleton.java
+++ src/main/java/br/com/desafiosfitness/config/MysqlSingleton.java
@@ -13,7 +13,7 @@ import java.sql.SQLException;
 public class MysqlSingleton {
 
     private static final String URL =
-            "jdbc:mysql://localhost:3306/desafios_fitness?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+            "jdbc:mysql://mysql:3306/desafios_fitness?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     private static final String USER = "desafios_user";
     private static final String PASSWORD = "desafios123";
 
